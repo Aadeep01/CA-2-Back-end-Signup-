11bba3c5a283546a83416133b07129a73cc6a7d2
Aadeep Kaur <aadeepkaur@yahoo.com>
2025-02-26 10:36:55 +0530

    56

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { signup } = require("../Controller/AuthController");
-const validateSignup = require("../Middleware/ValidateSignUp");
+const validateSignup = require("../Middleware/ValidateSignup");
 
 const router = express.Router();
 
